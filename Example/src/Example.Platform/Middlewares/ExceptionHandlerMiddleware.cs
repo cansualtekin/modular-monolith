ca51ae014eee1494b2656b8220c47e15fa679e60
Cansu Altekin <36486183+cansualtekin@users.noreply.github.com>
2022-06-05 15:22:44 +0300

    Global exception handler implemented

---
+++ Example/src/Example.Platform/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog;
+
+namespace Example.Platform.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger = Log.ForContext<ExceptionHandlerMiddleware>();
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                httpContext.Items.Add("exception", ex);
+                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                _logger.Error(ex, ex.Message);
+            }
+        }
+    }
+
+}
