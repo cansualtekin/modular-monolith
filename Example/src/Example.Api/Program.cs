6292b595f0b974e7e12fa0027d197084ffa7dc6b
Cansu Altekin <36486183+cansualtekin@users.noreply.github.com>
2022-06-05 14:15:57 +0300

    Swagger support implemented

--- Example/src/Example.Api/Program.cs
+++ Example/src/Example.Api/Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Swashbuckle.AspNetCore.Filters;
+using System.Reflection;
 
 namespace Example.Api
 {
@@ -13,18 +16,40 @@ namespace Example.Api
             builder.Services.AddControllers();
 
             // Versioning
-            builder.Services.AddApiVersioning(options => {
-                options.DefaultApiVersion = new ApiVersion(1,0);
+            builder.Services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
                 options.ReportApiVersions = true;
                 options.AssumeDefaultVersionWhenUnspecified = true;
             });
 
-            builder.Services.AddVersionedApiExplorer(options => {
+            builder.Services.AddVersionedApiExplorer(options =>
+            {
                 options.GroupNameFormat = "'v'VVV";
                 options.SubstituteApiVersionInUrl = true;
             });
 
+            // Swagger
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.ExampleFilters();
+            });
+
+            builder.Services.AddSwaggerExamplesFromAssemblies(Assembly.GetExecutingAssembly());
+
             var app = builder.Build();
+
+            // Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+            });
 
             // Configure the HTTP request pipeline.
 
