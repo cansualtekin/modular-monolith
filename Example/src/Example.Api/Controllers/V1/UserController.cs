e850ccdc31837d8c465d209cb23d51df8294a125
Cansu Altekin <36486183+cansualtekin@users.noreply.github.com>
2022-06-05 07:47:38 +0300

    Project structure created

---
+++ Example/src/Example.Api/Controllers/V1/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Example.Api.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
