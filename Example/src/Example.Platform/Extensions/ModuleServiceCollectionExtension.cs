7f88485162b74080c48dba5fabe11ef52e25cae0
Cansu Altekin <36486183+cansualtekin@users.noreply.github.com>
2022-06-07 21:55:15 +0300

    Module support implemented

---
+++ Example/src/Example.Platform/Extensions/ModuleServiceCollectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.Platform.Extensions
+{
+    public static class ModuleServiceCollectionExtension
+    {
+        public static IServiceCollection AddModule<TModule>(this IServiceCollection services)
+            where TModule : class, IModule
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.SetupInformation.ApplicationBase)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var module = Activator.CreateInstance<TModule>();
+
+            module.ConfigureServices(services, configuration);
+
+            return services;
+        }
+    }
+}
---
+++ Example/src/Example.Platform/IModule.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.Platform
+{
+    public interface IModule
+    {
+        IServiceCollection ConfigureServices(IServiceCollection services, IConfiguration configuration);  
+    }
+}
