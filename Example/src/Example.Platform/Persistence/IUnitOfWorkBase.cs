4b1e11dc021a0c219125428f9bdd8eeddc9b6455
Cansu Altekin <36486183+cansualtekin@users.noreply.github.com>
2022-06-07 21:39:49 +0300

    UnitOfWork pattern implemented

---
+++ Example/src/Example.Platform/Persistence/IUnitOfWorkBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Example.Platform.Persistence
+{
+    public interface IUnitOfWorkBase 
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Example/src/Example.Platform/Persistence/UnitOfWorkBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Example.Platform.Persistence
+{
+    public class UnitOfWorkBase : IUnitOfWorkBase
+    {
+        private readonly DbContext _dbContext;
+
+        public UnitOfWorkBase(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+           return await _dbContext.SaveChangesAsync(cancellationToken);   
+        }
+    }
+}
