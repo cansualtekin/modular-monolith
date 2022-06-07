0de5d4fc7164de872e1fa2ba6490db7b592a012d
Cansu Altekin <36486183+cansualtekin@users.noreply.github.com>
2022-06-07 20:41:53 +0300

    Entity base implemented

---
+++ Example/src/Example.Platform/Persistence/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Example.Platform.Persistence
+{
+    public class EntityBase<TKey>
+        where TKey : struct
+    {
+        [Key]
+        public TKey Id { get; set; }
+
+        public DateTime CreatedOn { get; }
+
+        public bool IsDeleted { get; set; }
+
+        public EntityBase()
+        {
+            CreatedOn = DateTime.UtcNow;   
+        }
+    }
+}
