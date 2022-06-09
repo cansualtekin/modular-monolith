3d89b321e1f9ae5ce41b6657da025ee5bbf64bcc
Cansu Altekin <36486183+cansualtekin@users.noreply.github.com>
2022-06-09 20:03:34 +0300

    User data context implemented

---
+++ Example/src/Example.User.Infrastructure/Persistence/UserDataContext.cs
@@ -0,0 +1,25 @@
+﻿using Example.User.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.User.Infrastructure.Persistence
+{
+    public class UserDataContext : DbContext
+    {
+        public DbSet<UserEntity>  Users { get; set; }
+        public UserDataContext(DbContextOptions<UserDataContext> options) : base(options)   
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
